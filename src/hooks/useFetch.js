eb0fc1d10f87eece141f9f64876613e283e27dd4
lazarok09 <lazarok09@gmail.com>
2021-08-21 09:34:04 -0300

    feat: error

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,15 +2,17 @@ import { useState } from "react";
 
 export const useFetch = () => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const refetchData = async (url) => {
     const dataRaw = await fetch(url);
     const dataJSON = await dataRaw.json();
 
     setData(dataJSON);
+    setIsLoading(false);
     return dataJSON;
   };
 
   // return
-  return [data, refetchData];
+  return [data, refetchData, isLoading];
 };
--- src/templates/Home/Home.jsx
+++ src/templates/Home/Home.jsx
@@ -6,31 +6,41 @@ import { Header } from "../../components/Header";
 import styles from "./styles.module.css";
 
 export const Home = () => {
-  const [data, refetchData] = useFetch();
+  const [data, refetchData, isLoading] = useFetch();
+  // a função foi passada para um estado para não causar loops
   const [refetechDataState] = useState(() => refetchData);
 
   const [isDisabled, setIsDisabled] = useState(false);
 
   // realiza nova requisição com valores dinâmicos alterados via botão
   useEffect(() => {
+    // limit faz a nossa requisição apresentar um número customizado de resultados
     let limit = "?offset=10&limit=10";
 
     refetechDataState("https://pokeapi.co/api/v2/pokemon" + limit);
-  }, [refetechDataState]);
 
-  // first button
+    if (!isLoading && !!data.ok) {
+      throw new Error("404");
+    }
+  }, [refetechDataState, isLoading, data.ok]);
+
+  // primeiro botão  -
   const handleNextPokemons = () => {
     if (data.previous !== null) {
       setIsDisabled(false);
     }
+    // dentro do *resultado da requisição armazenado no estado data* possui a chave .next que é a próxima requisição
     data.next.length > 0 && refetechDataState(data.next);
   };
   // second button
   const handlePrevPokemons = () => {
+    /* o if seleciona o estado do botão para desativado caso não possua uma 
+    requisição para voltar (no caso da primeira requisição mostrada em tela, não há previous no estado data) */
     if (data.previous === null) {
       setIsDisabled((isDisabled) => !isDisabled);
       return;
     }
+    // dentro do *resultado da requisição armazenado no estado data* possui a chave .previous que foi a requisição anterior
     data.previous.length > 0 && refetechDataState(data.previous);
   };
 
@@ -48,12 +58,12 @@ export const Home = () => {
             ))}
         </div>
         <div className={styles.buttonDiv} id="buttons">
-          <Button text={"Next pokmeons"} onButtonClicked={handleNextPokemons} />
           <Button
             disabled={isDisabled}
             text={"Preview pokemons"}
             onButtonClicked={handlePrevPokemons}
           />
+          <Button text={"Next pokmeons"} onButtonClicked={handleNextPokemons} />
         </div>
       </main>
     </>
